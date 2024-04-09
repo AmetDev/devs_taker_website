a9bc0dbf963dac6b32725aae8fb60d5929792831
AmetDev <ametsejdaliev@gmail.com>
2024-04-09 21:19:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Main from './MainPage/Main';
+
+function App() {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
---
+++ src/MainPage/Main.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import './custom-alert.css';
+
+import './Main.css';
+import imgSrc from '../assets/img/image.png';
+import fileApkRu from '../assets/files/ru/universal.apk';
+import fileApkEn from '../assets/files/en/universal.apk';
+
+function Main() {
+
+  const showAlertRu = () => {
+    confirmAlert({
+      title: 'Подтверждение',
+      message: 'Вы уверены, что хотите загрузить русскоязычную версию DevsTaker?',
+      buttons: [
+        {
+          label: 'Да',
+          onClick: () => {
+            const downloadLink = document.createElement('a');
+            downloadLink.href = fileApkRu;
+            downloadLink.download = 'universal.apk';
+            downloadLink.click();
+          }
+        },
+        {
+          label: 'Отмена',
+          onClick: () => {
+            console.log('Вы нажали "Отмена"');
+          }
+        }
+      ]
+    });
+  };
+
+
+  const showAlertEn = () => {
+    confirmAlert({
+      title: 'Подтверждение',
+      message: 'Вы уверены, что хотите загрузить англоязычную версию DevsTaker?',
+      buttons: [
+        {
+          label: 'Да',
+          onClick: () => {
+            const downloadLink = document.createElement('a');
+            downloadLink.href = fileApkEn;
+            downloadLink.download = 'universal.apk';
+            downloadLink.click();
+          }
+        },
+        {
+          label: 'Отмена',
+          onClick: () => {
+            console.log('Вы нажали "Отмена"');
+          }
+        }
+      ]
+    });
+  };
+  
+  return (
+    <>
+      <div className="container">
+        <img className="img" src={imgSrc} alt="ball" />
+        <div className="content">
+          <h1 className="title">DevsTaker</h1>
+          <p className="text-p">Попробуйте версии приложения на русском и английском:</p>
+          <div className="btn-div">
+            <button className="btn" onClick={showAlertRu}><b>Скачать</b></button>
+            <button className="btn" onClick={showAlertEn}>Download</button>
+          </div>
+        </div>
+    </div>
+    <div className="description">
+        <div className="text">
+            <p>Воспользуйтесь моментом, делайте</p>
+            <p>заметки с DevsTaker!</p>
+        </div>
+    </div>
+    </>    
+  );
+}
+
+export default Main;
